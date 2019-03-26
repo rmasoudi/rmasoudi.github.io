5c57e96f8a9ab8a633d34f2f97163528a01e4df8
unknown <rmrasoulmasoudi@gmail.com>
2019-03-26 11:07:43 +0430

    back button

--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,8 @@
 $$ = Dom7;
 PAGE_SIZE = 20;
+document.addEventListener("backbutton", function () {
+    alert("back button pressed");
+}, false);
 db = new loki('words.db', {
     autoload: true,
     autoloadCallback: databaseInitialize,
